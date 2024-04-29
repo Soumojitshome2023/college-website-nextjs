713d7794494768f92e56b9d088dc348df8f37586
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-04-29 21:19:19 +0530

    cors handling middleware added

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const adminRouter = require("./route/admin.route.js");
-const cookieParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
 dotenv.config(".env");
 
 const app = express();
+
+app.use(cors({
+    origin : process.env.FRONTEND_URI,
+    methods : ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}))
 
 app.use(express.json());
 
