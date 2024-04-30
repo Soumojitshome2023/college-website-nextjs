b9bcc36c0fd85e49ba8875f98a368fa247cbe8ff
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-04-30 20:57:09 +0530

    admin dashboard initiated

---
+++ client/src/app/admin/dashboard/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//============== Admin Dashborad ===============
+const Page = () => {
+  return (
+    <div>
+        <div className='w-full bg-blue-500 py-4 px-5'>
+            <span className='w-full flex text-white text-lg font-medium uppercase justify-end'>Welcome: <span>admin</span></span>
+        </div>
+        <div className='mx-auto mt-5 p-5 w-[90%] min-h-[65vh] border border-blue-500 rounded'>
+            hello
+        </div>
+    </div>
+  )
+}
+
+export default Page;
---
+++ client/src/components/admin/dashboard/DashboardCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//============ Dashboard Button Item =============
+
+export const DashboardCard = () => {
+  return (
+    <div>
+        Edit
+    </div>
+  )
+}
