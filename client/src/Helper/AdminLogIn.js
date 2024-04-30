2a65df026722b17ea9b87205f39e31dfd098e75b
Ritesh Pramanik <pramanikritesh907@gmail.com>
2024-04-30 14:32:32 +0530

    axios added

--- client/src/Helper/AdminLogIn.js
+++ client/src/Helper/AdminLogIn.js
@@ -1,23 +1,26 @@
+import axios from "axios";
 export const AdminLogIn = (email, password) => {
     return new Promise(async (resolve, reject) => {
         try {
             // Fetch API
             const fetchlink = process.env.NEXT_PUBLIC_SERVERURL;
-            let result = await fetch(fetchlink + "/api/v1/admin/login", {
-                method: "POST",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(
-                    {
-                        "email": email,
-                        "password": password,
-                    }
-                ),
-                withCredentials: true
-            });
-
-            const data = await result.json();
+            // let result = await fetch(fetchlink + "/api/v1/admin/login", {
+            //     method: "POST",
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify(
+            //         {
+            //             "email": email,
+            //             "password": password,
+            //         }
+            //     ),
+            //     withCredentials: true
+            // });
+            const { data } = await axios.post(fetchlink+"/api/v1/admin/login",{email,password},{
+                withCredentials : true
+            })
+            // const data = await result.json();
             resolve(data);
         } catch (error) {
             console.error('Error fetching data:', error);
