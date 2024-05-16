3f469d325641a6620a789ebd6d25001af28044c2
Soumojitshome2023 <soumojitshome2021@gmail.com>
2024-05-16 15:40:58 +0530

    V1.1.1 error fix

--- client/src/components/Home/EventCard/EventCard.js
+++ client/src/components/Home/EventCard/EventCard.js
@@ -1,14 +1,16 @@
-import AnimatedElement from '@/components/Common/Animation/AnimatedElement'
-import React, { useState } from "react";
+"use client"
 import Link from "next/link";
+import React, { useState } from "react";
+import AnimatedElement from '@/components/Common/Animation/AnimatedElement'
 
 export default function EventCard({ data }) {
 	const [ReadMore, setReadMore] = useState(false);
+
 	return (
 		<>
 			{/* Card  */}
 			<AnimatedElement className="p-2 my-4 mx-2 md:w-96 w-full">
-				<div className="rounded-lg overflow-hidden border-b-2 border-gray-700">
+				<div className="rounded-lg overflow-hidden border-b-2 border-gray-700 min-h-96">
 					<img className="md:h-48 h-40 w-full rounded-lg object-cover object-center" src={data.posterURL} alt="img" />
 					<div className="p-2">
 						{/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
